5d911abd9b00c63aee7fcc575f1417021876c332
Kayley-Galbraith <kaydorgal24@gmail.com>
2025-02-13 16:02:26 -0700

    Add files via upload
    Class 8 files

--- my_functions_lib.py
+++ my_functions_lib.py
@@ -1,6 +1,6 @@
 #!/usr/local/Anaconda2023/bin/python3.9
 
-
+import matplotlib.pyplot as plt
 import numpy as np
 def my_function(vector):
 	a = vector[0]
@@ -12,3 +12,8 @@ def my_function(vector):
 def y(x):
 	y = 2.0*x**3.0
 	return y
+
+def distance(x): #from class 8, something astronomy related
+	u = 5*np.log10(x*100)
+	return u
+
